93a7ff88de083cfa048c9d6867cbe6f3c98823cb
chrisnguyen123456 <tringuyen21072002@gmail.com>
2023-09-30 17:48:14 +0700

    week3

--- WEEK3/App.js
+++ WEEK3/App.js
@@ -1,13 +1,13 @@
 import { Button, Image, StyleSheet, Text, View, ViewBase } from "react-native";
 import Lab1_1 from "./component/Lab1_1";
-//import Lab1_2 from "./component/Lab1_2";
-//import Lab1_3 from "./component/Lab1_3";
-//import Lab1_4 from "./component/Lab1_4";
+import Lab1_2 from "./component/Lab1_2";
+import Lab1_3 from "./component/Lab1_3";
+import Lab1_4 from "./component/Lab1_4";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      { <Lab1_1 /> }
+      {/* <Lab1_1 /> */}
       {/* <Lab1_2 />; */}
       {/* <Lab1_3 />; */}
       <Lab1_4 />;
